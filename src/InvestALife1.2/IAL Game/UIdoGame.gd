6b48d88014a0bd3aecd1998ccc6b793e9abbebaf
FelipeSantosInteli <felipe.santos@sou.inteli.edu.br>
2022-02-25 14:55:37 -0300

    update

--- src/InvestALife1.2/IAL Game/UIdoGame.gd
+++ src/InvestALife1.2/IAL Game/UIdoGame.gd
@@ -5,3 +5,15 @@ func _ready():
 	ProjectSettings.get_setting("display/window/size/width")
 	ProjectSettings.get_setting("display/window/size/height")
 	
+	$barra_inferior/SubirBarra.visible = true
+	$barra_inferior/DescerBarra.visible = false
+
+func _process(delta):
+	if $barra_inferior/SubirBarra.visible == true && $barra_inferior/DescerBarra.visible == false && Input.is_action_just_released("subir_barra"):
+		$barra_inferior/AnimationPlayer.play("subir barra")
+		$barra_inferior/SubirBarra.visible = false
+		$barra_inferior/DescerBarra.visible = true		
+	elif $barra_inferior/SubirBarra.visible == false && $barra_inferior/DescerBarra.visible == true && Input.is_action_just_released("descer_barra"):
+		$barra_inferior/AnimationPlayer.play("descer barra")
+		$barra_inferior/DescerBarra.visible = false
+		$barra_inferior/SubirBarra.visible = true
--- src/InvestALife1.2/IAL Game/scenes/barra_inferior.gd
+++ src/InvestALife1.2/IAL Game/scenes/barra_inferior.gd
@@ -2,17 +2,16 @@ extends TextureRect
 
 func _ready() -> void:
 #	self.set_position(Vector2(30, 1792))
-	$SubirBarra.visible = true
-	$DescerBarra.visible = false
-
-func _process(delta):
-	if $SubirBarra.visible == true && $DescerBarra.visible == false && Input.is_action_just_released("subir_barra"):
-		$AnimationPlayer.play("subir barra")
-		$SubirBarra.visible = false
-		$DescerBarra.visible = true		
-	elif $SubirBarra.visible == false && $DescerBarra.visible == true && Input.is_action_just_released("descer_barra"):
-		$AnimationPlayer.play("descer barra")
-		$DescerBarra.visible = false
-		$SubirBarra.visible = true
-
-
+#	$SubirBarra.visible = false
+#	$DescerBarra.visible = true
+#
+#func _process(delta):
+#	if $SubirBarra.visible == true && $DescerBarra.visible == false && Input.is_action_just_released("subir_barra"):
+#		$AnimationPlayer.play("subir barra")
+#		$SubirBarra.visible = false
+#		$DescerBarra.visible = true		
+#	elif $SubirBarra.visible == false && $DescerBarra.visible == true && Input.is_action_just_released("descer_barra"):
+#		$AnimationPlayer.play("descer barra")
+#		$DescerBarra.visible = false
+#		$SubirBarra.visible = true
+	pass
