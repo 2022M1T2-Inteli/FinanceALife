f9a9198af394d9e30e31c9451bcbd29d7995551e
FelipeSantosInteli <99196112+FelipeSantosInteli@users.noreply.github.com>
2022-03-02 19:56:16 -0300

    update

--- src/InvestALife1.2/IAL Game/scenes/scripts/UIdoGame.gd
+++ src/InvestALife1.2/IAL Game/scenes/scripts/UIdoGame.gd
@@ -1,5 +1,16 @@
 extends Control
 var texto = false
+var escolhas_txt = [
+	"res://assets/text/text_escolhas/escolha1.tres",
+	"res://assets/text/text_escolhas/escolha2.tres",
+	"res://assets/text/text_escolhas/escolha3.tres"
+]
+
+func load_file(file_path):
+	var file = File.new()
+	file.open(file_path, file.READ)
+	var text = file.get_as_text()
+	return text
 
 func _ready():
 	ProjectSettings.get_setting("display/window/size/width")
@@ -15,6 +26,16 @@ func _ready():
 	$escolha1/select.hide()
 	$escolha2/select.hide()
 	$escolha3/select.hide()
+	
+	var escolha1 = escolhas_txt[0]
+	var escolha2 = escolhas_txt[1]
+	var escolha3 = escolhas_txt[2]
+	$escolha1/Label.text = load_file(escolha1)
+	$escolha2/Label2.text = load_file(escolha2)
+	$escolha3/Label3.text = load_file(escolha3)
+#	$escolha1/fechar.hide()
+#	$escolha2/fechar.hide()
+#	$escolha3/fechar.hide()
 	
 func _process(delta):
 
