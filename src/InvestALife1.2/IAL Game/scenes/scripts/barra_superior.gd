d08783e23eea3f154023ccad355e7aec1807950c
FelipeSantosInteli <felipe.santos@sou.inteli.edu.br>
2022-03-03 15:20:19 -0300

    update

--- src/InvestALife1.2/IAL Game/scenes/scripts/barra_superior.gd
+++ src/InvestALife1.2/IAL Game/scenes/scripts/barra_superior.gd
@@ -1,13 +1,18 @@
 extends TextureRect
 
-var nv_dinheiro = 0.00
+var max_dinheiro = 2000
+var dinheiro = 900
+#var nv_dinheiro = dinheiro*max_dinheiro
 var nv_energia = 0
 var nv_felicidade = 0
 var nv_divida = 0
 
 func _ready():
-	$nv_dinheiro.value = nv_dinheiro
+	$nv_dinheiro.max_value = max_dinheiro
+	$nv_dinheiro.value = dinheiro
 	$nv_energia.value = nv_energia
 	$nv_felicidade.value = nv_felicidade
 	$nv_divida.value = nv_divida
 	
+
+
