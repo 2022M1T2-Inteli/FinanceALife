ec6fd58c8f2aa1d3b117d392a4f6cbac6be0b059
marcelofeitoza <feitoza.marcelo7@gmail.com>
2022-03-22 17:09:14 -0300

    Pushing game over screen

---
+++ src/InvestALife1.2/IAL Game/scenes/scripts/GameOver.gd
@@ -0,0 +1,8 @@
+extends Control
+
+func _on_restart_pressed():
+	get_tree().change_scene("res://scenes/inicializador.tscn")
+
+func _on_sair_pressed():
+	get_tree().quit()
+
--- src/InvestALife1.2/IAL Game/scenes/scripts/inicializador.gd
+++ src/InvestALife1.2/IAL Game/scenes/scripts/inicializador.gd
@@ -1,7 +1,8 @@
 extends Control
 
 func _on_start_pressed():
-	get_tree().change_scene("res://scenes/new_mail.tscn")
+#	get_tree().change_scene("res://scenes/new_mail.tscn")
+	get_tree().change_scene("res://scenes/GameOver.tscn")
 
 func _on_sair_pressed():
 	get_tree().quit()
